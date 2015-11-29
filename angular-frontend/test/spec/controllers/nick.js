c870df3afd95dcaf2669d14bc312611f63d79f2b
Oleg Belousov <oleg.belousov@rocket-internet.de>
2015-11-29 16:49:14 +0100

    first commit

---
+++ angular-frontend/test/spec/controllers/nick.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('Controller: NickCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('appApp'));
+
+  var NickCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    NickCtrl = $controller('NickCtrl', {
+      $scope: scope
+    });
+  }));
+
+});
