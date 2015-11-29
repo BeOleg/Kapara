c82486438b681ca4d856cb38a559b3392cbd837c
bb420 <boaz@blocktrail.com>
2015-11-29 17:55:35 +0100

    angular ui router

--- angular-frontend/app/scripts/app.js
+++ angular-frontend/app/scripts/app.js
@@ -5,6 +5,24 @@ angular
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'btford.socket-io'
+    'btford.socket-io',
+    'ui.router'
   ])
-  .value('nickName', 'anonymous');
+  .value('nickName', 'anonymous')
+    .config(function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/");
+
+        $stateProvider
+            .state('home', {
+                url: "",
+                templateUrl: "views/home.html",
+                controller: 'SocketCtrl'
+            })
+            .state('home.list', {
+                url: "/list",
+                templateUrl: "partials/state1.list.html",
+                controller: function($scope) {
+                    $scope.items = ["A", "List", "Of", "Items"];
+                }
+            });
+    });
